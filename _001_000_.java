62f05b5b21e7a7b83822a85b5c7a0a7fd8532155
sejongPark <lifedesigner88@gmail.com>
2023-11-19 11:36:00 +0900

    2023.11.19 11시

---
+++ _001_000_.java
@@ -0,0 +1,74 @@
+// 001 나머지 구하기
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1 % num2;
+        return answer;
+    }
+}
+
+
+// 002 두 수의 곱
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1*num2;
+        return answer;
+    }
+}
+
+
+// 003 몫 구하기
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1/num2;
+        return answer;
+    }
+}
+
+
+// 004 숫자 비교하기
+class Solution {
+    public int solution(int num1, int num2) {
+        if(num1 == num2)
+            return 1;
+        return -1;
+    }
+}
+
+
+// 005 두 수의 차
+class Solution {
+    public int solution(int num1, int num2) {
+        return num1-num2;
+    }
+}
+
+
+// 006 나이출력
+class Solution {
+    public int solution(int age) {
+        int answer = 2022-age+1;
+        return answer;
+    }
+}
+
+
+// 007 두 수의 합
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1 + num2;
+        return answer;
+    }
+}
+
+
+// 008 두 수의 나눗셈
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = (num1 * 1000) / num2;
+        return answer;
+    }
+}
+
+
+// 009 각도기
+https://school.programmers.co.kr/learn/courses/30/lessons/120829
